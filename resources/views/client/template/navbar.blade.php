3cad9625438713c4506ce6915be2373b7c76d04e
Nghia <minhnghia2311@gmail.com>
2020-05-30 14:13:17 +0700

    nghia.le create template blog

---
+++ resources/views/client/template/navbar.blade.php
@@ -0,0 +1,28 @@
+<!-- Navigation -->
+<nav class="navbar navbar-inverse navbar-fixed-top">
+    <div class="container">
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle" data-toggle="collapse"
+                data-target="#bs-example-navbar-collapse-1">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="index.html"><img src="{{asset('client/images/logo.png')}}"
+                    alt="company logo" /></a>
+        </div>
+        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+            <ul class="nav navbar-nav navbar-right custom-menu">
+                <li><a href="index.html#home">Home</a></li>
+                <li><a href="index.html#about">About</a></li>
+                <li><a href="index.html#services">Services</a></li>
+                <li><a href="index.html#meet-team">Team</a></li>
+                <li><a href="index.html#portfolio1">Portofolio</a></li>
+                <li><a href="index.html#contact">Contact</a></li>
+                <li class="active"><a href="blog.html">Blog</a></li>
+                <li><a href="single-post.html">Single</a></li>
+            </ul>
+        </div>
+    </div>
+</nav>
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,5 @@
 Route::get('/', function () {
     return view('welcome');
 });
+Route::view('/blog', 'client.pages.blog');
+Route::view('/single-blog', 'client.pages.single_blog');
