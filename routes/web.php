de084b849645406be39747202c01e27372863f6d
Nghia <minhnghia2311@gmail.com>
2020-06-27 13:07:54 +0700

    nghia.le create route group

--- routes/web.php
+++ routes/web.php
@@ -20,9 +20,32 @@ Route::post('/xac-thuc', 'LoginController@login')->name('login');
 Route::get('/dang-xuat', 'LoginController@logout');
 
 Route::group(['middleware' => ['checkLogin']], function () {
-    Route::get('/hoc-tap', 'ForumController@index')->name('forum');
-    Route::get('/them-cau-hoi', 'QuestionController@create')->name('question.create');
-    Route::post('/them-cau-hoi', 'QuestionController@store')->name('question.store');
-    Route::view('/single-blog', 'client.pages.single_blog');
+    // forum
+    Route::group(['prefix' => 'hoc-tap'], function () {
+        
+        Route::get('/', 'ForumController@index')->name('forum');
+        Route::get('/bai-viet/{slug}', 'ForumController@show')->name('forum.show');
+        Route::get('/them-cau-hoi', 'QuestionController@create')->name('question.create');
+        Route::post('/them-cau-hoi', 'QuestionController@store')->name('question.store');
+    });
+    Route::group(['prefix' => 'chia-se'], function () {
+        
+    });
+    Route::group(['prefix' => 'cau-lac-bo'], function () {
+        
+    });
+    Route::group(['prefix' => 'doan-hoi'], function () {
+        
+    });
+    Route::group(['prefix' => 'tai-khoan'], function () {
+        
+    });
+    //lấy tất cả các messages, và sẽ có form để chat
+    Route::get('messages', 'MessageController@index');
     
+    //insert chat content vào trong database
+    Route::post('messages', 'MessageController@store');
+    
+    //lấy ra user hiện tại
+    Route::get('current-user', 'UserController@currentUser');
 });
